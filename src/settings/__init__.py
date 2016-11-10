1e3e71382e4d3f27802a6b8c40506bd6ffc8b009
mavriq <admin@mavriq.net>
2016-11-11 00:26:09 +0300

    * Добавил возможность работать на python26 (лучи ненависти в адрес centOS6) * Добавил возможность работать на python3 (все там будем)

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -3,8 +3,9 @@
 Универсальный Django settings.
 """
 
-from .settings import *
 import os
+import sys
+from .settings import *
 
 try:
     from .localsettings import fix_settings
@@ -12,24 +13,26 @@ except ImportError:
     try:
         from .localsettings import *
     except ImportError:
-        raise ImportError("WARNING: Can't load `{}`".format(
+        raise ImportError("WARNING: Can't load `{0}`".format(
             os.path.join(os.path.basename(os.path.abspath(__file__)), 'localsettings.py')))
 else:
     fix_settings(locals())
     del(fix_settings)
 
 if locals().get('SECRET_KEY') is None:
+    if 2 == sys.version_info.major:
+        FileNotFoundError = IOError
     _SECRET_FILE_NAME = os.path.join(BASE_DIR, 'private', 'secret.txt')
     try:
         SECRET_KEY = open(_SECRET_FILE_NAME).read().strip()
-    except IOError:
+    except FileNotFoundError:
         try:
             from django.utils.crypto import get_random_string
             chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
             SECRET_KEY = get_random_string(50, chars)
-            _SECRET_FILE = file(_SECRET_FILE_NAME, 'w')
+            _SECRET_FILE = open(_SECRET_FILE_NAME, 'w')
             _SECRET_FILE.write(SECRET_KEY)
             _SECRET_FILE.close()
             del(_SECRET_FILE, _SECRET_FILE_NAME, chars)
         except IOError:
-            Exception('Please, create a {} file with random characters to generate your secret key'.format(_SECRET_FILE_NAME))
+            Exception('Please, create a {0} file with random characters to generate your secret key'.format(_SECRET_FILE_NAME))
